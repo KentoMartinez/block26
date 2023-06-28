796f67b7d4fbdf1bb321830e8ae668d9de74b497
KentoMartinez <ayrtonmg4@gmail.com>
2023-06-27 22:09:00 -0700

    modified:   block26/src/App.jsx 	modified:   block26/src/components/ContactList.jsx 	new file:   block26/src/components/SelectedContact.jsx

--- block26/src/components/ContactList.jsx
+++ block26/src/components/ContactList.jsx
@@ -6,9 +6,9 @@ const dummyContacts = [
    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
  ];
- export default function ContactList() {
-   
-   const [contacts, setContacts] = useState(dummyContacts);
+ export default function ContactList({ setSelectedContactId }) { 
+
+  const [contacts, setContacts] = useState(dummyContacts);
    useEffect(() => {
       async function fetchContacts() {
         try {
@@ -37,7 +37,7 @@ const dummyContacts = [
               <td>Phone</td>
             </tr>
             {contacts.map((contact) => {
-          return <ContactRow key={contact.id} contact={contact} />;
+          return <ContactRow key={contact.id} contact={contact} setSelectedContactId={setSelectedContactId}/>;
         })}
           </tbody>
         </table>
